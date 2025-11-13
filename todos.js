f7a16dffb537ff13726d43b6653f288c5392803d
tal bendet <talbendet21@gmail.com>
2025-11-13 13:45:52 +0200

    refactor(.gitignore): add generated directory to ignore list for cleaner builds refactor(todos): remove outdated TODO comment for improved clarity

--- todos.js
+++ todos.js
@@ -1,6 +1,5 @@
 // TODO add web folder and react ts config
 // TODO add type folder for shared types
 // TODO fix env cnfigs and make sure they are optimal
-// TODO turbo run build?
 // TODO consider prima push when building a pros script,needed if there is no database
 // TODO edit turbo.json
