25c132ac162f10c65dca12475756240ecc777c28
tal bendet <talbendet21@gmail.com>
2025-11-13 13:17:34 +0200

    refactor(todos): update TODO comments for clarity and remove outdated entries refactor(turbo): remove unused migration tasks for cleaner configuration

--- todos.js
+++ todos.js
@@ -1,5 +1,5 @@
-// TODO go over scripts and fix any issues
 // TODO add web folder and react ts config
 // TODO add type folder for shared types
 // TODO fix env cnfigs and make sure they are optimal
 // TODO turbo run build?
+// TODO consider prima push when building a pros script,needed if there is no database
