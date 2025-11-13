0b9bdfdfd8f2b22fea8b06d67eb8faf617c37cd2
tal bendet <talbendet21@gmail.com>
2025-11-13 13:04:58 +0200

    refactor(todos): remove outdated TODO comments for clarity and focus

--- todos.js
+++ todos.js
@@ -1,9 +1,5 @@
 // TODO go over scripts and fix any issues
-// TODO add node js subpath imports and test
-// TODO check dev vs prod scripts
-// TODO go over tsconfig paths and make sure they are optimal
 // TODO add web folder and react ts config
 // TODO add type folder for shared types
 // TODO fix env cnfigs and make sure they are optimal
 // TODO turbo run build?
-// TODO consider adding tsconfig.build.json
