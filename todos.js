2773622fca35be405aee5686bcf5478890b67b67
tal bendet <talbendet21@gmail.com>
2025-11-13 14:27:52 +0200

    refactor(todos.js): remove outdated TODO comment for turbo.json

--- todos.js
+++ todos.js
@@ -2,4 +2,3 @@
 // TODO add type folder for shared types
 // TODO fix env cnfigs and make sure they are optimal
 // TODO consider prima push when building a pros script,needed if there is no database
-// TODO edit turbo.json
