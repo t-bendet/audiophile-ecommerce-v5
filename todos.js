1cc730e94d947070b57d085f6309b0c2a39a1653
tal bendet <talbendet21@gmail.com>
2025-11-13 10:13:35 +0200

    refactor(todos): reintroduce TODO comments for script improvements and optimizations

---
+++ todos.js
@@ -0,0 +1,6 @@
+// TODO go over scripts and fix any issues
+// TODO add node js subpath imports and test
+// TODO check dev vs prod scripts
+// TODO go over tsconfig paths and make sure they are optimal
+// TODO add web folder and react ts config
+// TODO add type folder for shared types
