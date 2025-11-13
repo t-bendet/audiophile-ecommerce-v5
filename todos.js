f14bb194ff5e344537fe97ac908f24574ffd17a7
tal bendet <talbendet21@gmail.com>
2025-11-13 11:26:54 +0200

    refactor(scripts): standardize script names and improve turbo configuration

--- todos.js
+++ todos.js
@@ -5,3 +5,5 @@
 // TODO add web folder and react ts config
 // TODO add type folder for shared types
 // TODO fix env cnfigs and make sure they are optimal
+// TODO turbo run build?
+// TODO consider adding tsconfig.build.json
