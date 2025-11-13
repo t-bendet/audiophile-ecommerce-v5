8206fc99fc79c434406d4d433a12e9923338cb4e
tal bendet <talbendet21@gmail.com>
2025-11-13 14:35:23 +0200

    refactor(todos.js): consolidate TODO comments for clarity and optimization

--- todos.js
+++ todos.js
@@ -1,4 +1,5 @@
+// TODO go over server changes and make sure everything is optimal
+// TODO add type folder for shared types ,zod schemas ,prisma types etc
 // TODO add web folder and react ts config
-// TODO add type folder for shared types
 // TODO fix env cnfigs and make sure they are optimal
 // TODO consider prima push when building a pros script,needed if there is no database
