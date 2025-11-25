de6b1b1ddac52398f0a997f682f9e49982461a2c
tal bendet <talbendet21@gmail.com>
2025-11-25 14:16:31 +0200

    refactor(validation): update Zod import and improve error message formatting

--- apps/server/src/middlewares/validation.middleware.ts
+++ apps/server/src/middlewares/validation.middleware.ts
@@ -1,12 +1,13 @@
-import { ZodSchema } from "zod";
+import Z from "zod";
 import { RequestHandler } from "express";
 import AppError from "../utils/appError.js";
 import catchAsync from "../utils/catchAsync.js";
 
 // * Middleware to validate request body against a Zod schema
 
-export const validateSchema = (schema: ZodSchema): RequestHandler =>
+export const validateSchema = (schema: Z.ZodSchema): RequestHandler =>
   catchAsync(async (req, _res, next) => {
+    console.log("object");
     const parsedRequest = schema.safeParse({
       params: req.params,
       body: req.body,
@@ -14,11 +15,7 @@ export const validateSchema = (schema: ZodSchema): RequestHandler =>
     });
 
     if (!parsedRequest.success) {
-      const message = `Unprocessable Content.The following variables are missing or invalid:
-    ${Object.entries(parsedRequest.error.flatten().fieldErrors)
-      .map(([k, v]) => `- ${k}: ${v}`)
-      .join("\n")}
-      `;
+      const message = `Unprocessable Content.The following variables are missing or invalid:"\n"${Z.prettifyError(parsedRequest.error)}`;
       return next(new AppError(message.trim(), 422));
     }
     return next();
