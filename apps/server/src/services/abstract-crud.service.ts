3b024888bbd87673abc80a394ca8a53ba4f3917b
tal bendet <talbendet21@gmail.com>
2025-11-29 12:10:49 +0200

    refactor: remove TODO comment regarding admin routes in AbstractCrudService

--- apps/server/src/services/abstract-crud.service.ts
+++ apps/server/src/services/abstract-crud.service.ts
@@ -135,5 +135,3 @@ export abstract class AbstractCrudService<
     if (!existed) throw new AppError("No document found with that ID", 404);
   }
 }
-
-// TODO handle // * ADMIN ROUTES (restricted to admin roles) and protected routes
