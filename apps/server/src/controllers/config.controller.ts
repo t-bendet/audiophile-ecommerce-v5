8bb61339522290203cc90c63b2c634fe0ef38373
tal bendet <talbendet21@gmail.com>
2025-12-10 13:41:19 +0200

    refactor: implement CRUD operations for config with validation and response handling

--- apps/server/src/controllers/config.controller.ts
+++ apps/server/src/controllers/config.controller.ts
@@ -1,29 +1,35 @@
-import { prisma } from "@repo/database";
+import {
+  createEmptyResponse,
+  createListResponse,
+  createSingleItemResponse,
+} from "@repo/domain";
 import { RequestHandler } from "express";
+import { configService } from "../services/config.service.js";
 import catchAsync from "../utils/catchAsync.js";
 
-export const createConfig: RequestHandler = catchAsync(async (req, res) => {
-  const { name, featuredProduct, showCaseProducts } = req.body;
-
-  const config = await prisma.config.create({
-    data: {
-      name,
-      featuredProduct,
-      showCaseProducts,
-    },
-  });
-
-  res.status(201).json({
-    status: "success",
-    data: config,
-  });
-});
+// TODO consider adding config id to env variables for easy access
 
 export const getConfig: RequestHandler = catchAsync(async (req, res) => {
-  const config = await prisma.config.findFirstOrThrow();
+  const result = await configService.getUniqueConfig();
+  res.status(200).json(createSingleItemResponse(result));
+});
 
-  res.status(201).json({
-    status: "success",
-    data: config,
-  });
+// * ADMIN CONTROLLERS
+
+export const createConfig: RequestHandler = catchAsync(async (req, res) => {
+  const dto = await configService.create(req.verified?.body);
+  res.status(201).json(createSingleItemResponse(dto));
+});
+
+export const updateConfig: RequestHandler = catchAsync(async (req, res) => {
+  const dto = await configService.update(
+    req.verified?.params.id,
+    req.verified?.body
+  );
+  res.status(200).json(createSingleItemResponse(dto));
+});
+
+export const deleteConfig: RequestHandler = catchAsync(async (req, res) => {
+  await configService.delete(req.verified?.params.id);
+  res.status(200).json(createEmptyResponse());
 });
