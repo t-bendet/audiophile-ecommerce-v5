7ac0fbad430395fa9ab19adee52f0630380b5989
tal bendet <talbendet21@gmail.com>
2025-12-10 21:02:19 +0200

    refactor: update user request handling to use user ID directly and simplify request schemas

--- apps/server/src/controllers/user.controller.ts
+++ apps/server/src/controllers/user.controller.ts
@@ -10,21 +10,22 @@ import catchAsync from "../utils/catchAsync.js";
 import AppError from "../utils/appError.js";
 
 export const getMe = (req: Request, _res: Response, next: NextFunction) => {
-  if (!req.verified || !req.verified.params.id) {
+  if (!req.user || !req.user.id) {
     throw new AppError("User ID not found in request", ErrorCode.UNAUTHORIZED);
   } else {
-    req.verified.params.id = req.user?.id!;
+    req.params.id = req.user?.id!;
+    next();
   }
 };
 
 // Get a single user
 export const getUser: RequestHandler = catchAsync(async (req, res, _next) => {
-  const dto = await userService.get(req.verified?.params.id);
+  const dto = await userService.get(req.verified?.params.id ?? req.params.id);
   res.status(200).json(createSingleItemResponse(dto));
 });
 
 export const deleteMe: RequestHandler = catchAsync(async (req, res, _next) => {
-  await userService.update(req.verified?.params.id, { active: false });
+  await userService.update(req.user!.id, { active: false });
   res.status(200).json(createEmptyResponse());
 });
 
@@ -56,10 +57,7 @@ export const updateUser: RequestHandler = catchAsync(
 );
 
 export const updateMe: RequestHandler = catchAsync(async (req, res, _next) => {
-  const dto = await userService.update(
-    req.verified?.params.id,
-    req.verified?.body
-  );
+  const dto = await userService.update(req.user!.id, req.verified?.body);
   res.status(200).json(createSingleItemResponse(dto));
 });
 
