2bb5962115932846741dbd752fc57300ddbbdfed
tal bendet <talbendet21@gmail.com>
2025-12-15 09:54:07 +0200

    refactor: streamline category fetching and remove unused types

--- apps/client/src/features/categories/api/get-categories.ts
+++ apps/client/src/features/categories/api/get-categories.ts
@@ -1,44 +1,18 @@
 // ** Get All Categories
 
 import { api } from "@/lib/api-client";
-import { TBaseHandler, TBaseRequestParams, TCategory } from "@/types/api";
-import { IdValidator } from "@/utils/validators";
+import { TBaseHandler, TBaseRequestParams } from "@/types/api";
+import {
+  CategoryGetAllResponse,
+  CategoryGetAllResponseSchema,
+} from "@repo/domain";
 import { queryOptions } from "@tanstack/react-query";
-import z from "zod";
 
-// TODO solve this workaround for  date string
-
-type TCategoryWithDateAsString = Omit<TCategory, "createdAt"> & {
-  createdAt: string;
-};
-
-type TGetCategories = TBaseHandler<TCategoryWithDateAsString[]>;
-
-// TODO make category union dynamic
-
-const CategorySchema = z.object({
-  id: IdValidator("product"),
-  createdAt: z.string(),
-  name: z.union([
-    z.literal("Headphones"),
-    z.literal("Earphones"),
-    z.literal("Speakers"),
-  ]),
-  v: z.number(),
-  thumbnail: z.object({
-    altText: z.string(),
-    ariaLabel: z.string(),
-    src: z.string(),
-  }),
-});
-
-const CategoriesSchema = z.array(CategorySchema) satisfies z.Schema<
-  TCategoryWithDateAsString[]
->;
-
-const getAllCategories: TGetCategories = async ({ signal }) => {
+const getAllCategories: TBaseHandler<CategoryGetAllResponse> = async ({
+  signal,
+}) => {
   const response = await api.get("/categories", { signal });
-  const result = CategoriesSchema.safeParse(response.data.data);
+  const result = CategoryGetAllResponseSchema.safeParse(response.data.data);
   if (result.success) {
     return result.data;
   } else {
