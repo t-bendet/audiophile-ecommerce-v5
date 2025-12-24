176e15ffa97424f1741b0c920d320b6e934dde18
tal bendet <talbendet21@gmail.com>
2025-12-24 14:04:27 +0200

    feat: implement AppError handling in FeaturedProductSection, ProductActions, and ProductPrice components

--- apps/client/src/features/products/components/featured-product-section/index.tsx
+++ apps/client/src/features/products/components/featured-product-section/index.tsx
@@ -1,12 +1,16 @@
 import { ResponsivePicture } from "@/components/ui/responsivePicture";
 import { getFeaturedProductQueryOptions } from "@/features/products/api/get-product";
 import ProductCard from "@/features/products/components/product-card";
+import { AppError, ErrorCode } from "@repo/domain";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 const FeaturedProductSection = () => {
   const { data: product } = useSuspenseQuery(getFeaturedProductQueryOptions());
   if (!product.data.images.featuredImage) {
-    throw new Error("Featured product does not have a featured image.");
+    throw new AppError(
+      "Featured product does not have a featured image.",
+      ErrorCode.COMPONENT_COMPOSITION_ERROR,
+    );
   }
 
   return (
