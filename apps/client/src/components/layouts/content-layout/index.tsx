5a6ddabc349df6500b269abeb9e017e95ec0eeef
tal bendet <talbendet21@gmail.com>
2026-01-13 21:27:04 +0200

    feat: add dropdown menu component and integrate user authentication features

--- apps/client/src/components/layouts/content-layout/index.tsx
+++ apps/client/src/components/layouts/content-layout/index.tsx
@@ -1,6 +1,8 @@
 import { Footer } from "@/components/layouts/content-layout/footer";
 import { Navbar } from "@/components/layouts/content-layout/nav-bar";
 import { getCategoriesQueryOptions } from "@/features/categories/api/get-categories";
+import { getUserQueryOptions } from "@/lib/auth";
+import { normalizeError } from "@/lib/errors/errors";
 import { QueryClient } from "@tanstack/react-query";
 import { LoaderFunctionArgs, Outlet } from "react-router";
 
@@ -17,7 +19,18 @@ const ContentLayout = () => {
 export const clientLoader =
   (queryClient: QueryClient) => async (_context: LoaderFunctionArgs) => {
     await queryClient.prefetchQuery(getCategoriesQueryOptions());
-    // console.log({context});
+    // TODO find a better approach then just calling  getUser to check auth status,will do for now
+    console.log(document.cookie, "check this");
+    try {
+      await queryClient.ensureQueryData(getUserQueryOptions());
+    } catch (error) {
+      const normalizedError = normalizeError(error);
+      if (normalizedError.code === "UNAUTHORIZED") {
+        // silently fail, user is not logged in
+      } else {
+        throw normalizedError;
+      }
+    }
     return null;
   };
 
