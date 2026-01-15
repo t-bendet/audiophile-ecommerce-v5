4f131a2007b27d94b2bd01565078b4a3d3b01130
tal bendet <talbendet21@gmail.com>
2026-01-15 12:24:59 +0200

    feat: implement user account routing and profile page, enhance user dropdown functionality

--- apps/client/src/components/layouts/content-layout/nav-bar/user-avatar-dropdown.tsx
+++ apps/client/src/components/layouts/content-layout/nav-bar/user-avatar-dropdown.tsx
@@ -1,4 +1,3 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -7,51 +6,93 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronsUpDown } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { paths } from "@/config/paths";
+import { UserDTO } from "@repo/domain";
+import { ChevronsUpDown, IdCardLanyard, LogInIcon, LogOut } from "lucide-react";
+import { NavLink } from "react-router";
 
-export function UserAvatarDropdown() {
-  // TODO demo purpose only,replace with actual user data
+function LoggedInUserDropdown({ user }: { user: UserDTO }) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button>
           <span className="sr-only">Open user menu</span>
-          {/* <p>Hello Tal</p> */}
-          <p>Hello Guest</p>
+          <p className="capitalize">hello {user.name}!</p>
           <ChevronsUpDown className="ml-auto size-4" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="bg-white text-black" align="start">
+      <DropdownMenuContent className="bg-white text-black" align="center">
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <Separator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Keyboard shortcuts
-            <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-          </DropdownMenuItem>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
 
         <DropdownMenuSeparator />
         <DropdownMenuItem>
           Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          <LogOut className="text-primary-700 ml-auto size-4" />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+function AnonymousUserDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button>
+          <span className="sr-only">Open anonymous menu</span>
+          <p className="capitalize">hello visitor!</p>
+          <ChevronsUpDown className="ml-auto size-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white text-black" align="center">
+        <DropdownMenuItem>
+          <NavLink
+            to={paths.auth.login.path}
+            className={({ isActive }) => (isActive ? "text-primary-500" : "")}
+          >
+            Log in
+          </NavLink>
+          <LogInIcon className="text-primary-500 ml-auto size-4" />
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <NavLink
+            to={paths.auth.signup.path}
+            className={({ isActive }) => (isActive ? "text-primary-500" : "")}
+          >
+            Sign up
+          </NavLink>
+          <IdCardLanyard className="text-primary-500 ml-auto size-4" />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function UserDropdown() {
+  // TODO demo purpose only,replace with actual user data
+  const user = {
+    id: "693a9c77b06139c25ec24112",
+    name: "admin",
+    email: "admin@example.com",
+    role: "ADMIN" as const,
+    emailVerified: false,
+    createdAt: new Date("2023-10-11T10:20:30Z"),
+    v: 0,
+  };
+
+  return !user ? (
+    <LoggedInUserDropdown user={user} />
+  ) : (
+    <AnonymousUserDropdown />
   );
 }
