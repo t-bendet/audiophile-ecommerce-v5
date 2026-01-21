3ba9b8161c33e66d135105253a0d228c0e060af0
tal bendet <talbendet21@gmail.com>
2026-01-21 13:04:09 +0200

    minor

--- apps/client/src/app/routes/account/profile.tsx
+++ apps/client/src/app/routes/account/profile.tsx
@@ -1,6 +1,6 @@
 export default function ProfilePage() {
   return (
-    <main className="flex min-h-svh flex-col items-center justify-center bg-black p-6 md:p-10">
+    <main className="flex min-h-svh flex-col items-center justify-center p-6 text-neutral-900 md:p-10">
       <h1 className="text-3xl font-bold">Profile Page</h1>
       <p className="mt-4 text-lg">This is the user profile page.</p>
     </main>
