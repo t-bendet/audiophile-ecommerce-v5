2312dc3f38205b47f989b3808be4f2e14f3a09b6
tal bendet <talbendet21@gmail.com>
2026-01-27 12:05:16 +0200

    minor

--- apps/client/src/components/layouts/root-layout.tsx
+++ apps/client/src/components/layouts/root-layout.tsx
@@ -10,6 +10,7 @@ import { LoaderFunctionArgs, Outlet, ScrollRestoration } from "react-router";
 // TODO add AUTH_STATUS_QUERY_KEY based loading bar here to cover all pages
 // TODO add auth loader state handling here(or globally), e.g. show a top loading bar when user auth state is being checked/refetched
 
+// TODO turn auth loader to a hook and use it in nav-bar to show user related loading states there
 export function RootLayout() {
   const isFetching = useIsFetching({ queryKey: [USER_QUERY_KEY] });
   const isMutating = useIsMutating({ mutationKey: [USER_QUERY_KEY] });
