cb19d56d18526f0a8c386d6737c47c6a00b723ba
tal bendet <talbendet21@gmail.com>
2026-01-27 11:30:25 +0200

    feat: refactor account pages layout and navigation, enhance user experience

--- apps/client/src/app/routes/account/orders.tsx
+++ apps/client/src/app/routes/account/orders.tsx
@@ -1,20 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { TabsContent } from "@/components/ui/tabs";
 import { QueryClient } from "@tanstack/react-query";
 import { LoaderFunctionArgs } from "react-router";
 
 export default function OrdersPage() {
   return (
-    <TabsContent value="orders" className="mt-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Orders</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <h1>Orders</h1>
-        </CardContent>
-      </Card>
-    </TabsContent>
+    <Card>
+      <CardHeader>
+        <CardTitle>Orders</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <h1>Orders Information</h1>
+      </CardContent>
+    </Card>
   );
 }
 
@@ -22,35 +19,3 @@ export const clientLoader =
   (_queryClient: QueryClient) => async (_context: LoaderFunctionArgs) => {
     return null;
   };
-
-//   <Container>
-//   <h1 className="mb-6 text-3xl font-bold">Account Settings</h1>
-//   <p className="text-muted-foreground mb-4">Logged in as: {data.email}</p>
-//   <Tabs defaultValue="profile" className="w-full">
-//     <TabsList>
-//       <TabsTrigger value="profile">Profile</TabsTrigger>
-//       <TabsTrigger value="security">Security</TabsTrigger>
-//       <TabsTrigger value="billing">Billing</TabsTrigger>
-//     </TabsList>
-//     <TabsContent value="profile" className="mt-4">
-//       <Card>
-//         <CardHeader>
-//           <CardTitle>Profile Information</CardTitle>
-//         </CardHeader>
-//         <CardContent>
-//           <h1>Profile</h1>
-//         </CardContent>
-//       </Card>
-//     </TabsContent>
-//     <TabsContent value="security" className="mt-4">
-//       <Card>
-//         <CardHeader>
-//           <CardTitle>Change Password</CardTitle>
-//         </CardHeader>
-//         <CardContent>
-//           <h1>Security</h1>
-//         </CardContent>
-//       </Card>
-//     </TabsContent>
-//   </Tabs>
-// </Container>
--- apps/client/src/app/routes/account/security.tsx
+++ apps/client/src/app/routes/account/security.tsx
@@ -1,20 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { TabsContent } from "@/components/ui/tabs";
 import { QueryClient } from "@tanstack/react-query";
 import { LoaderFunctionArgs } from "react-router";
 
 export default function SecurityPage() {
   return (
-    <TabsContent value="security" className="mt-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Profile Information</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <h1>Profile Information</h1>
-        </CardContent>
-      </Card>
-    </TabsContent>
+    <Card>
+      <CardHeader>
+        <CardTitle>Security Information</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <h1>Security Information</h1>
+      </CardContent>
+    </Card>
   );
 }
 
